83dd8ed054aee222201a42fbe0433b6418fc7ac6
IngAlim2023 <enrique.porras@udea.edu.co>
2024-08-26 23:10:18 -0500

    Seguridad aplicada en django, falta react

--- apiFul/src/settings.py
+++ apiFul/src/settings.py
@@ -12,6 +12,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 
 from pathlib import Path
 
+#Ocultar archivos importantes: pip install python-decouple:
+
+from decouple import config
+
+
+
+
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
 
@@ -20,7 +27,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-cc80eh(!+&jyftx7$)wb2-m-qhq#r-loq9dlbz6=z$-(4c5aeh'
+SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
