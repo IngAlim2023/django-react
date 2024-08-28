fd4c3448f5d55935d8eaeb6875ad19f3943c50cf
IngAlim2023 <enrique.porras@udea.edu.co>
2024-08-28 07:53:35 -0500

    depencias y requirements, style select

--- cliente/src/components/SelectDoc.jsx
+++ cliente/src/components/SelectDoc.jsx
@@ -26,7 +26,7 @@ export function SelectDoc({ control }) {
       control={control}
       rules={{ required: true }}
       render={({ field }) => (
-        <Select
+        <Select className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
           {...field}
           options={options}
           placeholder="Seleccione tipo documento"
